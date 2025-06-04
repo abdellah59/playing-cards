c22d6c5a06a1838dbbc232a0afb012c3e4fd2f6a
Abdellah <abdellah.ait59@gmail.com>
2025-06-04 11:43:49 +0200

    feat: integrate playing card component into the main app template

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { Component } from '@angular/core';
+import { PlayingCard } from "./components/playing-card/playing-card";
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl:'./app.css'
+  styleUrl:'./app.css',
+  imports: [PlayingCard]
 })
 
 export class App {
